ac857000cb102457ea47b40af608e3eb0f0e7912
kharle14 <enriquezkharl14@gmail.com>
2024-03-26 00:58:25 +0800

    Search service seed data

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,6 +1,4 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
-using SearchService.Models;
+using SearchService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,17 +11,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-await DB.InitAsync(
-    "SearchDB",
-    MongoClientSettings.FromConnectionString(
-        builder.Configuration.GetConnectionString("MongoDbConnection")
-    )
-);
-
-await DB.Index<Item>()
-    .Key(x => x.Make, KeyType.Text)
-    .Key(x => x.Model, KeyType.Text)
-    .Key(x => x.Color, KeyType.Text)
-    .CreateAsync();
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
