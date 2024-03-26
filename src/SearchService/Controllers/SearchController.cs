1cb113e129401d33c553f3fe8d9e173e4f26882d
kharle14 <enriquezkharl14@gmail.com>
2024-03-27 00:52:18 +0800

    Added search by make

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    {
+        var query = DB.Find<Item>();
+
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+
+        var results = await query.ExecuteAsync();
+
+        return results;
+    }
+}
